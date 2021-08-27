32bbde8781cf0290abcbd89724e6f9e60e27fb65
gleb <sergeyaspect1@gmail.com>
2021-08-27 17:43:15 +0800

    ádd while

--- application.py
+++ application.py
@@ -1,1 +1,13 @@
-print(16*10,19**2)
+bobux=2
+bobux= bobux+4
+tix=10
+tix= tix+3
+very_nice=tix+bobux
+if tix == bobux:
+    very_nice=True#if tix = bobux
+while True:
+    if very_nice == True:
+        break
+print(bobux,tix)
+
+    
